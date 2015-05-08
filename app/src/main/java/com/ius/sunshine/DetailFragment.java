07a23e4a6aafeffd390ea186e8d6bc61dbb4205d
Ayush Maharjan <ius.maharjan@gmail.com>
2015-05-09 00:35:22 +0545

    Share Action added.

---
+++ app/src/main/java/com/ius/sunshine/DetailFragment.java
@@ -0,0 +1,69 @@
+package com.ius.sunshine;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailFragment extends Fragment {
+
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+    private static final String HashTag = "#SunshineApp";
+
+    private String forecastStr;
+
+    public DetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if(mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareIntent());
+        } else {
+            Log.d(LOG_TAG, "ShareActionProvider is null");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Intent intent = getActivity().getIntent();
+
+        View rootView =  inflater.inflate(R.layout.fragment_detail, container, false);
+
+        if (intent != null && intent.hasExtra(MainActivity.EXTRA_MESSAGE)) {
+            forecastStr = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+        }
+        return rootView;
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, forecastStr + " " + HashTag);
+        return shareIntent;
+    }
+}
