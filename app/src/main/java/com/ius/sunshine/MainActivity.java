8440aff199c6b500541188bda80af858d11a7517
Ayush Maharjan <ius.maharjan@gmail.com>
2015-05-08 23:56:52 +0545

    Menu item added to open map of current location.

--- app/src/main/java/com/ius/sunshine/MainActivity.java
+++ app/src/main/java/com/ius/sunshine/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ius.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,6 +14,7 @@ import android.view.MenuItem;
 public class MainActivity extends ActionBarActivity {
 
     public final static String EXTRA_MESSAGE = "com.ius.sunshine.MESSAGE";
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +46,29 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             startActivity(new Intent(this,SettingsActivity.class));
             return true;
+        } else if (id == R.id.action_open_map) {
+            openPreferredLocationInMap();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void openPreferredLocationInMap() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        intent.setData(geoLocation);
+
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Could not find application to open " + location);
+        }
+    }
 }
